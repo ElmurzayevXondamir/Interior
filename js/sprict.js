c2496194d48f799f821e75ac805e8cd76d10791a
ElmurzayevXondamir <elmurzayevxondamir@gmail.com>
2023-11-09 22:36:10 +0500

    first commit

---
+++ js/sprict.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".mySwiper-two", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".next-2",
+    prevEl: ".prev-2",
+  },
+  loop: true,
+});
